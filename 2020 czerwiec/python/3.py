1f8e5db626b02daced0628d14a3861f6015bac56
woonder <77258100+woonderer@users.noreply.github.com>
2023-04-03 15:27:37 +0200

    Update 3.py

--- 2020 czerwiec/python/3.py
+++ 2020 czerwiec/python/3.py
@@ -3,7 +3,7 @@ with open("pary.txt", "r") as f:
     file = f.read().strip().split('\n')
     for i in file:
         n, s = i.split()
-        pary.append((int(n), s))
+        if n == len(s): pary.append((int(n), s))
 
 pary.sort()
 print(*pary[0])
